df9042a673349f5ac8869b0d1eabf7b0bd229ae6
pujamalla127 <puja.malla91@stu-mail.jjay.cuny.edu>
2026-09-08 22:44:45 -0400

    Add files via upload
    Assignment 1 - hello world program

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+// puja malla - assignment 1
+
+#include <iostream>
+using namespace std;
+int main(){
+    string name; 
+    cout << "hello world!" << endl;
+    cout << "what is your name?" ;
+    cin >> name;
+    cout << "nice to meet you," << name << "!" << endl;
+    
+    return 0;
+}
